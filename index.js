9333b3da21b9dc625e90174c87a82dc864d6e51b
arthur lelieur <arthur.lelieur2@gmail.com>
2025-11-27 11:46:09 +0100

    ex de traitement très long synchrone

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
+document.getElementById("Action").addEventListener("click", function(){
+    alert("Action button clicked!")
+});
 
 console.log("Hello World!");
 
+let a = 0;
+for( let i = 0; i < 500000000; i++)
+{
+    a += i ;
+}
+
+console.log("Fin a =", a);
